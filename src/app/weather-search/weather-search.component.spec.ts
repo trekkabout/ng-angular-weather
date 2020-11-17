3ea571419825bda634edb0a9f1f6d636302541c7
trekkabout <robertstephens225@hotmail.com>
2020-11-17 08:01:40 -0600

    package updates

--- src/app/weather-search/weather-search.component.spec.ts
+++ src/app/weather-search/weather-search.component.spec.ts
@@ -1,4 +1,4 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 
 import { WeatherSearchComponent } from './weather-search.component';
 
@@ -6,7 +6,7 @@ describe('WeatherSearchComponent', () => {
   let component: WeatherSearchComponent;
   let fixture: ComponentFixture<WeatherSearchComponent>;
 
-  beforeEach(async(() => {
+  beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
       declarations: [ WeatherSearchComponent ]
     })
